c009624894a91f7a5ef3051153f6361d6f3cfd68
Skor <13skorpium@gmail.com>
2021-03-18 19:34:13 +0100

    Player 1 wins return Fifteen-Love

--- game/GAME.py
+++ game/GAME.py
@@ -1,4 +1,17 @@
 # This is a sample Python script.
 
-def get_score():
-    return "Love-All"
+class Game:
+
+    def __init__(self, player1: str, player2: str):
+        self._player1 = player1
+        self._player2 = player2
+        self._wins = ""
+
+    def wins_point(self, player: str):
+        self._wins = player
+
+    def get_score(self) -> str:
+        if self._wins == self._player1:
+            return "Fifteen-Love"
+        return "Love-All"
+
--- test/test_game.py
+++ test/test_game.py
@@ -5,10 +5,24 @@ sys.path.append('../')
 
 from game_tennis.game.GAME import *
 
+def create_game() -> Game:
+    return Game("Player1", "Player2")
+
 def test_gameStart():
     #ARRANGE
+    game = create_game()
     #ACT
-    result = get_score()
+    result = game.get_score()
 
     #ASSERT
     assert result == "Love-All"
+
+def test_P1onePoint_ThenFifteenLove():
+    #ARRANGE
+    game = create_game()
+    #ACT - Player 1 wins one point
+    game.wins_point("Player1")
+    result = game.get_score()
+
+    #ASSERT
+    assert result == "Fifteen-Love"
